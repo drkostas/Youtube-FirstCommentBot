927885389df4f0df763d88373529537317271469
drkostas <georgiou.kostas94@gmail.com>
2021-06-09 17:51:13 -0400

    Started building the YoutubeMysqlDatastore class #4

--- youbot/datastore/__init__.py
+++ youbot/datastore/__init__.py
@@ -1,6 +1,6 @@
 """Cloudstore sub-package of YoutubeCommentBot."""
 
-from .mysql_datastore import MySqlDatastore
+from .mysql_datastore import YoutubeMySqlDatastore
 
 __author__ = "drkostas"
 __email__ = "georgiou.kostas94@gmail.com"
